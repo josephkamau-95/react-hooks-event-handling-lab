934295532fb0659dde6bc61bd0a9179428977b2f
Joseph Mwangi <josymuramu@gmail.com>
2025-05-23 13:36:01 +0300

    completed this assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,22 @@
 // Code Keypad Component Here
+import { useState } from 'react';
 
 function Keypad (){
+    const [password, setPassword] = useState('');
+    
+    function handleChange (event) {
+        setPassword(event.target.value);
+        console.log('Entering password...');
+    }
     return (
-        <div></div>
+        <div>
+            <input 
+            type="password" 
+            value={password}
+            placeholder="Enter your password"
+            onChange= {handleChange} 
+            />
+        </div>
     )
 }
 
